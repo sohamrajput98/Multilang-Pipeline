db5dd1efa0166e64e55eafc47eebb825128b889a
Soham <sohamrw098@gmail.com>
2025-10-04 20:41:49 +0000

    java: commit 4.4 - Adjusted configuration in application.properties

--- src/java/Main.java
+++ src/java/Main.java
@@ -52,3 +52,4 @@ public class Main {
 // Commit 4.1: Optimized list processing methods
 // Commit 4.2: Removed unused imports
 // Commit 4.3: Fixed null pointer issues
+// Commit 4.4: Adjusted configuration in application.properties
