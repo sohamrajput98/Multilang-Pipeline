fbb63a493d9a4b890a451454c31c608796f2b7f6
Soham <sohamrw098@gmail.com>
2025-10-04 20:42:49 +0000

    java: commit 4.24 - Enhanced logging and debug statements

--- src/java/Main.java
+++ src/java/Main.java
@@ -72,3 +72,4 @@ public class Main {
 // Commit 4.21: Adjusted configuration in application.properties
 // Commit 4.22: Adjusted configuration in application.properties
 // Commit 4.23: Added exception handling in main class
+// Commit 4.24: Enhanced logging and debug statements
