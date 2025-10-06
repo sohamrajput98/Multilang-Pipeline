a6808efa58e3d24fbe541869e4575082f8d75e1f
Soham <sohamrw098@gmail.com>
2025-10-06 20:27:27 +0000

    java: commit 6.9 - Added exception handling in main class

--- src/java/Main.java
+++ src/java/Main.java
@@ -86,3 +86,4 @@ public class Main {
 // Commit 6.6: Fixed null pointer issues
 // Commit 6.7: Updated Javadoc and README
 // Commit 6.8: Fixed null pointer issues
+// Commit 6.9: Added exception handling in main class
