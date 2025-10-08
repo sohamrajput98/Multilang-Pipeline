3bdf71410fd17ab8f11720022416bd02e59d704c
Soham <sohamrw098@gmail.com>
2025-10-08 14:15:01 +0000

    ruby: commit 7.11 - Refactored methods for readability

--- scripts/ruby/main.rb
+++ scripts/ruby/main.rb
@@ -163,3 +163,4 @@ puts "Sum of data: #{sum}"
 # Commit 7.8: Removed unused variables
 # Commit 7.9: Updated documentation and README
 # Commit 7.10: Refactored methods for readability
+# Commit 7.11: Refactored methods for readability
