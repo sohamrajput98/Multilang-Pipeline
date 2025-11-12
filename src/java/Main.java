99a2d8a882a458d4dadba2f8a7cc141828e71fd2
Soham <sohamrw098@gmail.com>
2025-11-12 16:04:34 +0000

    java: commit 15.3 - Adjusted configuration in application.properties

--- src/java/Main.java
+++ src/java/Main.java
@@ -99,3 +99,4 @@ public class Main {
 // Commit 6.19: Improved thread handling in modules
 // Commit 15.1: Removed unused imports
 // Commit 15.2: Improved thread handling in modules
+// Commit 15.3: Adjusted configuration in application.properties
