97b46b57cd336994a4c0ca9e06c40ddf9ba14f85
Soham <sohamrw098@gmail.com>
2025-12-01 15:07:21 +0000

    cpp: commit 23.13 - Fixed memory leak in helper functions

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -419,3 +419,4 @@ int main() {
 // Commit 23.10: Removed unused header files
 // Commit 23.11: Enhanced logging for debugging
 // Commit 23.12: Fixed memory leak in helper functions
+// Commit 23.13: Fixed memory leak in helper functions
